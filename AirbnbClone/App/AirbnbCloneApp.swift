4e2b725de395f4ad3d97f151caa2511dc55e0647
Pratik Ghadge <pratikhg.2001@gmail.com>
2025-09-21 00:37:05 +0530

    [iOS] feat: initial project skeleton

---
+++ AirbnbClone/App/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Pratik Haridas Ghadge on 21/09/25.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
