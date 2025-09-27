d55b2d9c45fa8682d5e218e82132b3bff4816494
Pratik Ghadge <pratikhg.2001@gmail.com>
2025-09-28 01:47:28 +0530

    [iOS] feat: Implement WhishlistView

---
+++ AirbnbClone/Core/Wishlist/WishlistView.swift
@@ -0,0 +1,46 @@
+//
+//  WishlistView.swift
+//  AirbnbClone
+//
+//  Created by Pratik Haridas Ghadge on 28/09/25.
+//
+
+import SwiftUI
+
+struct WishlistView: View {
+	var body: some View {
+		NavigationStack {
+			VStack(alignment: .leading, spacing: 32) {
+				VStack(alignment: .leading, spacing: 4) {
+					Text("Log in to view your wishlists")
+						.font(.headline)
+
+					Text("You can create, view or edit wishlists once you've logged in.")
+						.font(.footnote)
+
+				}
+
+				Button {
+					print("Log in")
+				} label: {
+					Text("Log in")
+						.font(.subheadline)
+						.fontWeight(.semibold)
+						.frame(width: 340)
+						.foregroundColor(.white)
+						.padding()
+						.background(.pink)
+						.cornerRadius(8)
+				}
+				Spacer()
+			}
+			.padding()
+			.navigationBarTitle("Wishlist")
+		}
+
+	}
+}
+
+#Preview {
+	WishlistView()
+}
